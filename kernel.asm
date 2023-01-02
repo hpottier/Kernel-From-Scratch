c854c038786c66ed0c3ca4588a5604de03edb08e
hpottier <hpottier@student.42.fr>
2023-01-02 17:21:08 +0100

    correction newline

--- kernel.asm
+++ kernel.asm
@@ -88,6 +88,7 @@ terminal_putchar:
     jne .cursor_moved
  
     mov dh, 0
+.terminal_putchar_cont:
     inc dl
  
     cmp dl, VGA_HEIGHT
@@ -103,8 +104,7 @@ terminal_putchar:
 
 .new_line:
     xor dh, dh
-    inc dl
-    jmp .cursor_moved
+    jmp .terminal_putchar_cont
 
  
 ; IN = cx: length of string, ESI: string location
